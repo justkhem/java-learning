4f7424cc1118bd1862a200f63eb81c0dc703e115
Khem Raj <khemrajrai5710@gmail.com>
2026-03-11 20:48:12 +0900

    Create Main.java

---
+++ variables/Main.java
@@ -0,0 +1,23 @@
+// Variables & Data Types practice
+
+public class Main {
+    public static void main(String[] args) {
+        
+        // String
+        String name = "Khem Raj";
+        
+        // Integer
+        int age = 20;
+        
+        // Double
+        double height = 1.70;
+        
+        // Boolean
+        boolean isLearning = true;
+        
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(isLearning);
+    }
+}
